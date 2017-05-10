a7b20d73df9c4afb1a40bbff5745dfb5f1a6844d
wz汪政 <wang.zheng@ctrip.com>
2017-05-10 15:19:47 +0800

    init

---
+++ com/ctrip/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2017/5/10 13:31
+@Author  : wang.zheng@ctrip.com
+@File    : __init__.py.py
+"""
---
+++ com/ctrip/zenlp/bigram_hmm.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2017/5/10 15:13
+@Author  : wang.zheng@ctrip.com
+@File    : bigram_hmm.py
+"""
